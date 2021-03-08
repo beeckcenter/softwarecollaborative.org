ad5ee8aa2465f33cb3f76ddc1d956ba5897d02d8
Waldo Jaquith <waldo@jaquith.org>
2021-03-08 16:44:22 -0500

    Add a debugging step

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,7 @@ echo -e "URL\tstates" | tee /tmp/states.csv >/dev/null
 grep -E "\| states" ./*.md |sed 's/\.md:|[[:blank:]]*states[[:blank:]]*\|[[:blank:]]*/\t/' >> /tmp/states.csv
 
 cd ..
+
+# Make sure these are being generated at build time
+cp /tmp/states.csv . states.csv
+cp /tmp/states.csv . projects.csv
