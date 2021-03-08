8c974d5002c4e413667b807eb8c6ee8a9b68f12f
waldoj <waldoj@users.noreply.github.com>
2021-03-08 21:59:21 +0000

    deploy: bd908ff965c74888cec771caadf1ac9f8fdf4bac

--- state-data.sh
+++ state-data.sh
@@ -4,11 +4,11 @@ cd cooperatives || exit
 
 # Generate TSV of every project and their URL slugs
 echo -e "URL\ttitle" | tee /tmp/projects.csv >/dev/null
-grep -E "^# " ./*.md |sed 's/.md:#/\t/' >> /tmp/projects.csv
+grep -E "^# " *.md |sed 's/.md:#/\t/' >> /tmp/projects.csv
 
 # Generate TSV of every project's URL slug and their states
 echo -e "URL\tstates" | tee /tmp/states.csv >/dev/null
-grep -E "\| states" ./*.md |sed 's/\.md:|[[:blank:]]*states[[:blank:]]*\|[[:blank:]]*/\t/' >> /tmp/states.csv
+grep -E "\| states" *.md |sed 's/\.md:|[[:blank:]]*states[[:blank:]]*\|[[:blank:]]*/\t/' >> /tmp/states.csv
 
 cd ..
 
